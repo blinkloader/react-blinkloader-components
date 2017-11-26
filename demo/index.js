054c1a849abc8630c1f4c198f820b9a844c4ac76
Max Makarochkin <mms.post@gmail.com>
2017-11-26 23:28:58 +0700

    img and provider

--- demo/index.js
+++ demo/index.js
@@ -6,7 +6,7 @@ import {render} from 'react-dom';
 import { Img, BlinkloaderProvider } from '../src/index';
 
 const App = () => {
-  return <Img src='https://pbs.twimg.com/profile_images/921702184210849792/sTQGwPtn_400x400.jpg' />;
+  return <Img src='https://www.common.com/static/images/community/community-5@2x.jpg' />;
 };
 
 render(<BlinkloaderProvider userId={process.env.BLINKLOADER_USER_ID} token={process.env.BLINKLOADER_DEV_TOKEN}>
