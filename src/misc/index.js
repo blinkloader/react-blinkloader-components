c4d166e111596dd407afe4ae49d01824327912ae
Max Makarochkin <mms.post@gmail.com>
2019-01-13 01:48:01 -0800

    update dependencies

--- src/misc/index.js
+++ src/misc/index.js
@@ -1,4 +1,4 @@
-const blinkloaderVersion = '2.0.5';
+const blinkloaderVersion = '2.0.6';
 const noBlinkloaderJs = 'Blinkloader Error! Couldn\'t optimize assets: missing "https://cdn.blinkloader.com/blinkloader-' + blinkloaderVersion + '.min.js" in page head.';
 
 const noBlinkloaderProjectId = 'Blinkloader can not render images without a project id. Make sure that all of your components are wrapped in BlinkloaderProvider with appropriate settings.';
